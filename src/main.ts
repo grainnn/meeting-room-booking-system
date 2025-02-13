a63557da85719846509634b3a969f2e2ee081278
谷雨 <guyu@t3caic.com>
2025-02-13 15:15:43 +0800

    自定义interceptor格式化resposne

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { FormatResponseInterceptor } from './format-response.interceptor';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,6 +19,8 @@ async function bootstrap() {
   SwaggerModule.setup('doc', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new FormatResponseInterceptor());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
